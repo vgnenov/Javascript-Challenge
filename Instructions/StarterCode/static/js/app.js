5dc7d957c9b6c829bbea3999b77799924c05a3a4
vgnenov <65702831+vgnenov@users.noreply.github.com>
2020-10-13 01:08:40 -0500

    baby steps

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+const tbody= d3.select('tbody');
+//build table
+
+function buildTable(data){
+    tbody.html("");
+    data.forEach((item) => {
+        var newRow=tbody.append('tr');
+        Object.values(item).forEach((val)=> {
+            newRow.append('td').text(val)
+        });
+    });
+};
+
