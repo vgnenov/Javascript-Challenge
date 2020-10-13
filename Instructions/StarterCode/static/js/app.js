374be157c9741b1885217112470ae9e0fe6af194
vgnenov <65702831+vgnenov@users.noreply.github.com>
2020-10-13 14:49:07 -0500

    working filter function

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -33,8 +33,68 @@ tableData.forEach((SpookyVisit) => {
 //var button = d3.select("#click-me");
 var button = d3.select("#filter-btn");
 
-var inputField = d3.select("#input-field");
+// var inputField = d3.select("#input-field");
 
+// Select the upvote and downvote buttons
+// var upvote = d3.select(".upvote");
+// var downvote = d3.select(".downvote");
+var form = d3.select("form");
+
+// grab references to the input element and the output div
+// var text = d3.select("#text");
+// var output = d3.select(".output");
+
+// // Function to reverse a string
+// function reverseString(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// // Function to handle input change
+// function handleChange(event) {
+//   // grab the value of the input field
+//   var inputText = d3.event.target.value;
+
+//   // reverse the input string
+//   var reversedInput = reverseString(inputText);
+
+//   // Set the output text to the reversed input string
+//   output.text(reversedInput);
+// }
+
+// text.on("change", handleChange);
+form.on("submit", runEnter);
+
+function buildTable(data){
+            tbody.html("");
+            data.forEach((item) => {
+                var newRow=tbody.append('tr');
+                Object.values(item).forEach((val)=> {
+                    newRow.append('td').text(val)
+                });
+            });
+        };
+
+
+function runEnter() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    var inputValue = inputElement.property("value");
+    var filteredData = tableData.filter((data) => data.datetime === inputValue);
+    filteredData.forEach((SpookyVisit) => {
+        var row = tbody.append("tr");
+        Object.entries(SpookyVisit).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+      console.log(filteredData);
+      buildTable(filteredData);
+
+    
+
+      //buildTable(filteredData);
+};
 
 // function buildTable(data){
 //         tbody.html("");
